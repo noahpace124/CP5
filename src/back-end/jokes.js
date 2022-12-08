449e3ef45108c60541a4cf6ddfc2a3d8363a681c
gquigley1313 <108026669+gquigley1313@users.noreply.github.com>
2022-12-07 18:50:59 -0800

    Update jokes.js

--- src/back-end/jokes.js
+++ src/back-end/jokes.js
@@ -11,6 +11,129 @@ const jokes = [
     writer: "Server",
     joke: "What's brown and sticky? A stick."
 }
+
+{
+    writer: "Server",
+    joke: "What do kids play when their mom is using the phone? Bored games."
+}
+{
+    writer: "Server",
+    joke: "What do you call an ant who fights crime? A vigilANTe!"
+}
+{
+    writer: "Server",
+    joke: "Why are snails slow? Because they’re carrying a house on their back."
+}
+{
+    writer: "Server",
+    joke: "What’s the smartest insect? A spelling bee!"
+}
+{
+    writer: "Server",
+    joke: "What does a storm cloud wear under his raincoat? Thunderwear."
+}
+{
+    writer: "Server",
+    joke: "What is fast, loud and crunchy? A rocket chip."
+}
+{
+    writer: "Server",
+    joke: "How does the ocean say hi? It waves!"
+}
+{
+    writer: "Server",
+    joke: "Why did the teddy bear say no to dessert? Because she was stuffed."
+}
+{
+    writer: "Server",
+    joke: "Why did the soccer player take so long to eat dinner? Because he thought he couldn’t use his hands."
+}
+{
+    writer: "Server",
+    joke: "Name the kind of tree you can hold in your hand? A palm tree!"
+}
+{
+    writer: "Server",
+    joke: "What did Yoda say when he saw himself in 4k? HDMI"
+}
+{
+    writer: "Server",
+    joke: "What’s Thanos’ favorite app on his phone? Snapchat."
+}
+{
+    writer: "Server",
+    joke: "Sandy’s mum has four kids; North, West, East. What is the name of the fourth child? Sandy, obviously!"
+}
+{
+    writer: "Server",
+    joke: "Why is a football stadium always cold? It has lots of fans!"
+}
+{
+    writer: "Server",
+    joke: "What do you call two bananas on the floor? Slippers."
+}
+{
+    writer: "Server",
+    joke: "Why do ducks have feathers on their tails? To cover their buttquacks."
+}
+{
+    writer: "Server",
+    joke: "A plane crashed in the jungle and every single person died. Who survived? Married couples."
+}
+{
+    writer: "Server",
+    joke: "Why can’t you ever tell a joke around glass? It could crack up."
+}
+{
+    writer: "Server",
+    joke: "What do you call a Star Wars droid that takes the long way around? R2 detour."
+}
+{
+    writer: "Server",
+    joke: "How do you stop an astronaut’s baby from crying? You rocket."
+}
+{
+    writer: "Server",
+    joke: "Why did the scarecrow win a Nobel prize? Because she was outstanding in her field."
+}
+{
+    writer: "Server",
+    joke: "How do you know when a bike is thinking? You can see their wheels turning."
+}
+{
+    writer: "Server",
+    joke: "How did the baby tell his mom he had a wet diaper? He sent her a pee-mail."
+}
+{
+    writer: "Server",
+    joke: "When does a joke become a “dad” joke? When the punchline is a parent."
+}
+{
+    writer: "Server",
+    joke: "How much did the man sell his dead batteries for? Nothing, they were free of charge!"
+}
+{
+    writer: "Server",
+    joke: "How do you make a lemon drop? Let it fall from the tree."
+}
+{
+    writer: "Server",
+    joke: "Why are basketball courts always wet? Because the players dribble."
+}
+{
+    writer: "Server",
+    joke: "How do billboards talk? Sign language."
+}
+{
+    writer: "Server",
+    joke: "What do you call a fish without an eye? A fsh.
+
+"
+}
+{
+    writer: "Server",
+    joke: "What do you call a tired bull? A bulldozer."
+}
 ];
 
 module.exports = jokes;
